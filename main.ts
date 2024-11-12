1bb16508e9a05ca3fef07a861d9393f3315c54d8
Allen Ye <38345792@qq.com>
2024-11-12 15:41:37 +0800

    up

---
+++ main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+ 
+const config: StorybookConfig = {
+  framework: '@storybook/react-webpack5',
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  core: {
+    builder: {
+      name: '@storybook/builder-webpack5',
+      options: {
+        fsCache: true,
+        lazyCompilation: true,
+      },
+    },
+  },
+};
+ 
+export default config;

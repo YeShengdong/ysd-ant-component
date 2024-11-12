4b12fd3d4bd779be71fe13121985d7840b6b08db
Allen Ye <38345792@qq.com>
2024-11-12 16:30:37 +0800

    up

--- stories/Header.jsx
+++ stories/Header.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-import { Button } from './components';
 import './header.css';
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
@@ -33,12 +32,12 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
             <span className="welcome">
               Welcome, <b>{user.name}</b>!
             </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
+            <button size="small" onClick={onLogout} label="Log out" />
           </>
         ) : (
           <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <button size="small" onClick={onLogin} label="Log in" />
+            <button primary size="small" onClick={onCreateAccount} label="Sign up" />
           </>
         )}
       </div>

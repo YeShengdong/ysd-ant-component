7158ac2716ef6fc48314d881f43ca8020f6ef21c
Allen Ye <38345792@qq.com>
2024-11-12 17:20:15 +0800

    配置less

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,11 +10,51 @@ const config = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
-    "@storybook/addon-styling-webpack"
+    "@storybook/addon-styling-webpack",
   ],
   framework: {
     name: "@storybook/react-webpack5",
-    options: {},
+    options: {
+      rules: [
+        // Replaces any existing Sass rules with given rules
+        {
+          test: /\.less$/i,
+          use: [
+            "style-loader",
+            "css-loader",
+            {
+              loader: "less-loader",
+              options: { implementation: require.resolve("less") },
+            },
+          ],
+        },
+      ],
+    },
+  },
+  webpackFinal: async (config) => {
+    const lessLoaderChain = {
+      test: /\.less$/,
+      use: [
+        "style-loader",
+        "css-loader",
+        {
+          loader: "less-loader",
+          options: {
+            lessOptions: {
+              javascriptEnabled: true,
+            },
+          },
+        },
+      ],
+    };
+    const oneOfRule = config.module.rules.find((rule) => !!rule.oneOf);
+    if (oneOfRule) {
+      oneOfRule.oneOf.unshift(lessLoaderChain);
+    } else {
+      config.module.rules.unshift(lessLoaderChain);
+    }
+
+    return config;
   },
 };
 export default config;
